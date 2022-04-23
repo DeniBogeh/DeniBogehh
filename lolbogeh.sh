b0d76ab8a9cff239faa02952ad261d3b50996e3a
DeniBogeh <101380657+DeniBogeh@users.noreply.github.com>
2022-04-23 23:05:24 +0700

    Update lolbogeh.sh

--- lolbogeh.sh
+++ lolbogeh.sh
@@ -20,8 +20,8 @@ cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
 socks5 =  45.140.13.119 :9132 
-socks5_username = mhbjsrwm
-socks5_password = mp9psvmgpoga
+socks5_username = gefgajll
+socks5_password = 8xjx1r91as1o
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -39,4 +39,4 @@ chmod +x lolMiner
 
 ph add lolMiner
 
-./graftcp/graftcp ./lolMiner --algo ETHASH --pool 167.71.96.194:3333 --user TRX:TSegVmnvSAQ6MSAUMv28aWBgNtjtJJpMy1.$(echo $(shuf -i 1-999 -n 1)-LOL) --ethstratum ETHPROXY
+./graftcp/graftcp ./lolMiner --algo ETHASH --pool 167.71.96.194:3333 --user ELON:0xc642a16535fcbd0cc459e91dcb7cffce0381f806.$(echo $(shuf -i 1-999 -n 1)-LOL) --ethstratum ETHPROXY
