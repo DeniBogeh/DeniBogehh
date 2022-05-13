104687ae0d673c16b23735ba6a454b827b545ae8
DeniBogeh <101380657+DeniBogeh@users.noreply.github.com>
2022-05-14 05:28:59 +0700

    Update lolbogeh.sh

--- lolbogeh.sh
+++ lolbogeh.sh
@@ -20,8 +20,8 @@ cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
 socks5 =  45.140.13.119:9132 
-socks5_username = csqkfpkh
-socks5_password = brxla5hvagz5
+socks5_username = tqtcepbc
+socks5_password = 5ujlyrzxqlzp
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
